dca99f18bc170e08bf90768601a25971e2d45bf4
kubbi <nicoladelaroche@gmail.com>
2025-02-18 14:55:27 +0100

    fix: does not remove .git anymore

--- reset_server.sh
+++ reset_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Array of folders to keep
-KEEP_FOLDERS=(".git", "client_resourcepacks" "packages")
+KEEP_FOLDERS=(".git" "client_resourcepacks" "packages")
 
 # Ensure we are in the correct directory
 cd "$(dirname "$0")" || exit 1
